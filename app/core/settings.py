8f1c92a7bdbe140732baff5ae2c74408fd428ba2
KenanBek <mail@kenanbek.me>
2014-09-28 03:47:35 +0500

    Grappelli temporally disabled due to login problem in Ubuntu.

--- app/core/settings.py
+++ app/core/settings.py
@@ -25,7 +25,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'grappelli',
+    #'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
