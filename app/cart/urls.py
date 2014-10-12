1748716866362db4ec2de8d45be2e023f0506c90
KenanBek <mail@kenanbek.me>
2014-10-12 09:52:19 +0500

    Initial product page. Not finished.

--- app/cart/urls.py
+++ app/cart/urls.py
@@ -4,5 +4,7 @@ from django.views.generic import RedirectView
 urlpatterns = patterns('cart.views',
     url(r'^$', RedirectView.as_view(url='/cart/index', permanent=True), name='index'),
     url(r'^index/$', 'index', name='cart_index'),
+
+    url(r'^product/(?P<product_id>[0-9]+)/$', 'product', name='cart_product'),
 )
 
--- app/cart/views.py
+++ app/cart/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+
 import models
 
 
 def index(request, template='bootstrap3/cart/index.html', context={}):
     last_products = models.Product.objects.all()
     context['last_products'] = last_products
+    return render(request, template, context)
+
+
+def product(request, product_id, template='bootstrap3/cart/product.html', context={}):
+    product_item = models.Product.objects.get(pk=product_id)
+    context['product_item'] = product_item
     return render(request, template, context)
 
