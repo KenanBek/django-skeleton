a6bc0e59cd35a387d05e4f0bd7033a8a54294280
KenanBek <mail@kenanbek.me>
2014-10-13 16:40:21 +0500

    Template changes.

--- app/account/urls.py
+++ app/account/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('account.views',
-    url(r'^$', RedirectView.as_view(url='/account/index', permanent=True), name='index'),
+    url(r'^$', RedirectView.as_view(url='/account/index', permanent=True)),
     url(r'^index/$', 'index', name='account_index'),
     url(r'^login/$', 'login', name='account_login'),
     url(r'^register/$', 'register', name='account_register'),
--- app/cart/urls.py
+++ app/cart/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('cart.views',
-    url(r'^$', RedirectView.as_view(url='/cart/index', permanent=True), name='index'),
+    url(r'^$', RedirectView.as_view(url='/cart/index', permanent=True)),
     url(r'^index/$', 'index', name='cart_index'),
 
     url(r'^product/(?P<product_id>[0-9]+)/$', 'product', name='cart_product'),
