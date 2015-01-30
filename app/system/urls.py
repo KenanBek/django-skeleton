ed89e40eb7e408fba86adb7f8adb1b8086ac9c44
KenanBek <mail@kenanbek.me>
2015-01-30 18:36:00 +0400

    Removed unneeded parts of system. Added localization for system view and templates.

--- app/system/urls.py
+++ app/system/urls.py
@@ -1,9 +1,4 @@
-from django.conf.urls import patterns, url, include
-
-info_urlpatterns = patterns('system.info',
-    url(r'^request/$', 'request_info', name='system_info_request'),
-    url(r'^localization/$', 'localization_info', name='system_info_localization'),
-)
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('system.views',
     # Error pages
@@ -11,7 +6,5 @@ urlpatterns = patterns('system.views',
     url(r'^error/403/$', 'error_403', name='error_403'),
     url(r'^error/404/$', 'error_404', name='error_404'),
     url(r'^error/500/$', 'error_500', name='error_500'),
-    # System info pages
-    url(r'^info/', include(info_urlpatterns)),
 )
 
