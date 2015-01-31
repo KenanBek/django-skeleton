efa906062fc02ca78b092da9bdd30f347bf8c81f
KenanBek <mail@kenanbek.me>
2015-01-31 04:27:06 +0400

    Item published status applied.

--- app/website/logic.py
+++ app/website/logic.py
@@ -5,19 +5,19 @@ import models
 
 
 def get_page(page_slug):
-    return get_object_or_404(models.Page, slug=page_slug, status=models.PUBLISHED)
+    return get_object_or_404(models.Page, slug=page_slug, status=models.ITEM_STATUS_PUBLISHED)
 
 
 def load_pages():
-    return models.Page.objects.filter(status=models.PUBLISHED).all()
+    return models.Page.objects.filter(status=models.ITEM_STATUS_PUBLISHED).all()
 
 
 def get_post(post_id, post_slug):
-    return get_object_or_404(models.Post, pk=post_id, slug=post_slug, status=models.PUBLISHED)
+    return get_object_or_404(models.Post, pk=post_id, slug=post_slug, status=models.ITEM_STATUS_PUBLISHED)
 
 
 def load_posts():
-    return models.Post.objects.filter(status=models.PUBLISHED).order_by('-modified_at').all()
+    return models.Post.objects.filter(status=models.ITEM_STATUS_PUBLISHED).order_by('-modified_at').all()
 
 
 class SearchResult:
@@ -32,9 +32,9 @@ class SearchResult:
 def search(term):
     pages = models.Page.objects.filter(Q(title__contains=term) | Q(content__contains=term))
     posts = models.Post.objects.filter((Q(title__contains=term)
-                                 | Q(short_content__contains=term)
-                                 | Q(full_content__contains=term))
-                                & Q(status=models.PUBLISHED))
+                                        | Q(short_content__contains=term)
+                                        | Q(full_content__contains=term))
+                                       & Q(status=models.ITEM_STATUS_PUBLISHED))
     return SearchResult(pages, posts)
 
 
