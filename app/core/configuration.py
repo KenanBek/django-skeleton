56559ef01a3be16c2d729be3942340fbbb889859
Kenan Bek <mail@kenanbek.me>
2015-04-22 17:35:53 +0500

    Refactore of configuration.py (closes #90).

--- app/core/configuration.py
+++ app/core/configuration.py
@@ -1,9 +1,9 @@
-import os
+# import os
 import sys
 
 
 def configure():
-    os.system("export LANG='en_US.UTF-8'")
-    os.system("export LC_ALL='en_US.UTF-8'")
+    # os.system("export LANG='en_US.UTF-8'")
+    # os.system("export LC_ALL='en_US.UTF-8'")
     sys.setrecursionlimit(10000)
 
