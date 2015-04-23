cbb2a3c056e64bb8342587ee097a4d571bae46b4
Kenan Bek <mail@kenanbek.me>
2015-04-23 11:20:30 +0500

    Refactored: init command.

--- app/system/management/commands/init.py
+++ app/system/management/commands/init.py
@@ -9,61 +9,89 @@ from django.conf import settings
 class Command(NoArgsCommand):
     help = "Initialize development environment with initial data."
 
+    def ds_print(self, str):
+        self.stdout.write('')
+        self.stdout.write('>>> DJANGO-SKELETON: ' + str)
+        self.stdout.write('')
+
     def handle_noargs(self, **options):
         base_dir = settings.BASE_DIR
 
         # DEBUG: Remove current database file
-        try:
-            os.remove(settings.DATABASES['default']['NAME'])
-            self.stdout.write('Database removed')
-        except OSError:
-            pass
-
-        # DEBUG: Remove media and logs if DEBUG
         if settings.DEBUG:
             try:
-                shutil.rmtree(os.path.join(base_dir, 'files', 'public', 'static'), ignore_errors=True)
-                shutil.rmtree(os.path.join(base_dir, 'files', 'public', 'media'), ignore_errors=True)
+                self.ds_print('Trying to remove database file...')
+                os.remove(settings.DATABASES['default']['NAME'])
+                self.ds_print('Database removed')
+            except Exception as e:
+                self.ds_print('Error on removing database file')
+                self.ds_print('ERROR: ' + e.message)
+
+        # DEBUG: Remove static, media and log files
+        if settings.DEBUG:
+            try:
+                self.ds_print('Trying to remove static, media and log files...')
+                shutil.rmtree(os.path.join(base_dir, 'files', 'public', 'static'))
+                shutil.rmtree(os.path.join(base_dir, 'files', 'public', 'media'))
                 os.remove(os.path.join(base_dir, 'files', 'logs', 'django.log'))
                 os.remove(os.path.join(base_dir, 'files', 'logs', 'application.log'))
                 os.remove(os.path.join(base_dir, 'files', 'logs', 'logic.log'))
-                self.stdout.write('Public, media and log files removed')
             except Exception as e:
-                self.stdout.write('ERROR: ' + str(e))
+                self.ds_print('Error on removing static, media and log files')
+                self.ds_print('ERROR: ' + str(e))
 
         # DEBUG: Remove migration files
         if settings.DEBUG:
             try:
+                self.ds_print('Trying to remove migration files...')
                 shutil.rmtree(os.path.join(base_dir, 'system', 'migrations'), ignore_errors=True)
                 shutil.rmtree(os.path.join(base_dir, 'account', 'migrations'), ignore_errors=True)
                 shutil.rmtree(os.path.join(base_dir, 'website', 'migrations'), ignore_errors=True)
                 shutil.rmtree(os.path.join(base_dir, 'cart', 'migrations'), ignore_errors=True)
-                self.stdout.write('Migration files removed')
+                self.ds_print('Migration files removed')
             except Exception as e:
-                self.stdout.write('ERROR: ' + str(e))
+                self.ds_print('Error on removing migration files')
+                self.ds_print('ERROR: ' + str(e))
 
         # Create (or recreate in case of DEBUG) initial migration files
-        management.call_command('makemigrations', 'system')
-        management.call_command('makemigrations', 'account')
-        management.call_command('makemigrations', 'website')
-        management.call_command('makemigrations', 'cart')
-        management.call_command('migrate', interactive=False)
-
+        try:
+            self.ds_print('Trying to create migration scripts...')
+            management.call_command('makemigrations', 'system')
+            management.call_command('makemigrations', 'account')
+            management.call_command('makemigrations', 'website')
+            management.call_command('makemigrations', 'cart')
+            management.call_command('migrate', interactive=False)
+            self.ds_print('Migration scripts created')
+        except Exception as e:
+            self.ds_print('Error on creating migration scripts')
+            self.ds_print('ERROR: ' + str(e))
 
         # DEBUG: Superuser and Fixtures
         if settings.DEBUG:
-            # Superuser
-            management.call_command('createsuperuser', username='admin', email='admin@host.local')
-            # Fixtures
-            management.call_command('loaddata', 'debug_account.json')
-            management.call_command('loaddata', 'debug_website.json')
-            management.call_command('loaddata', 'debug_cart.json')
+            try:
+                self.ds_print('Trying to create superuser and load development fixtures...')
+                # Superuser
+                management.call_command('createsuperuser', username='admin', email='admin@host.local')
+                # Fixtures
+                management.call_command('loaddata', 'debug_account.json')
+                management.call_command('loaddata', 'debug_website.json')
+                management.call_command('loaddata', 'debug_cart.json')
+                self.ds_print('Superuser created and fixtures loaded')
+            except Exception as e:
+                self.ds_print('Error on superuser and fixtures creation')
+                self.ds_print('ERROR: ' + str(e))
 
 
-        # DEBUG: Collect static
+        # DEBUG: Collect static files
         if not settings.DEBUG:
-            management.call_command('collectstatic', interactive=False)
+            try:
+                self.ds_print('Trying to collect static files...')
+                management.call_command('collectstatic', interactive=False)
+                self.ds_print('Static files collected')
+            except Exception as e:
+                self.ds_print('Error on collecting static files')
+                self.ds_print('ERROR: ' + str(e))
 
         # Done
-        self.stdout.write('Successfully finished.')
+        self.ds_print('Successfully finished.')
 
