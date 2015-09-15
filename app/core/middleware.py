2d93beb60a63d706db0492e60504a6ef13a4ea35
Kenan Bek <mail@kenanbek.me>
2015-09-15 09:50:46 +0500

    core middleware and context refactored, readme updated

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -5,4 +5,5 @@ class CoreMiddleware(object):
     def process_request(self, request):
         page_logic = PageLogic(request)
         page_logic.store_request()
+        return None  # Continue processing request
 
